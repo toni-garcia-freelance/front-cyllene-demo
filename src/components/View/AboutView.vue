5d336c37a1b5d4275fbed55822903e391b13c36f
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-06-17 18:13:37 +0200

    About view

--- src/components/View/AboutView.vue
+++ src/components/View/AboutView.vue
@@ -1,3 +1,32 @@
 <template>
-    About View
+    <div class="flex flex-col justify-center items-center mt-10 mx-5">
+        <h1 class="text-3xl text-center">Description du projet</h1>
+        <p class="mt-2 text-center">
+            Ce projet est une démo pour l'entreprise Cyllène.
+            <br>Il a été réalisé par Toni GARCIA pour un misssion de développeur full-stack VueJS / Laravel.
+            <br>
+            <br>Voici les technologies utilisées et les notions abordées dans ce projet :
+
+            <br><br>
+            <h3 class="text-xl"> En Front-End </h3>  
+            <ul class="list-disc list-inside">
+                <li>VueJS</li>
+                <li>Tailwind CSS</li>
+                <li>Axios</li>
+                <li>Pinia Store</li>
+                <li>Authentification</li>
+                <li>Appel API vers le back</li>
+            </ul>
+
+            <br><br>
+            <h3 class="text-xl"> En Back-End </h3>  
+            <ul class="list-disc list-inside">
+                <li>Laravel</li>
+                <li>Eloquent</li>
+                <li>Création de modèle, factory, seeder, controller, ...</li>
+                <li>Utilisation de NOVA pour l'administration</li>
+                <li>Mise en place d'une API minimal</li>
+            </ul>
+        </p>
+    </div> 
 </template>
